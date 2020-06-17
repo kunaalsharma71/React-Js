4912af1558c94e693294562d7feaec2c2a16b35b
Kunaal Sharma <KunaalSharma80@gmail.com>
2020-06-17 11:51:31 +0530

    Second Assig. Complete

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponents';
+import AboutUs from "./AboutComponent";
 import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -52,6 +53,7 @@ class Main extends Component{
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />  
           <Route path="/menu/:dishId" component={DishWithId} />
+          <Route path="/aboutus" component={() => <AboutUs leaders={this.state.leaders} />} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
